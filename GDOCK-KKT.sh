2b427a4060f26a8606dcc84d0a5027018d66c530
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-04-23 17:15:33 +0800

    Update GDOCK-KKT.sh

--- GDOCK-KKT.sh
+++ GDOCK-KKT.sh
@@ -10,16 +10,13 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# '修改默内核'
-# sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/ipq40xx/Makefile         # ipq40xx，内核：5.4，内核为5.10
- 
 # '修改默认IP'
 
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 
 # '修改默认主机名'
-sed -i 's/ImmortalWrt/GDOCK-KKT/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/GDOCK-KKT/g' package/base-files/files/bin/config_generate
 
 # 删除 '访问时间控制'
 rm -rf feeds/luci/applications/luci-app-accesscontrol 
@@ -37,19 +34,12 @@ git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 # git clone https://github.com/gdck/luci-app-control-weburl.git package/luci-app-control-weburl
 
 # '添加argon-config 使用最新argon
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-app-argon-config
-rm -rf feeds/kenzok/luci-theme-argon
-rm -rf feeds/kenzok/luci-app-argon-config
+# rm -rf package/lean/luci-theme-argon
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
 # '添加luci-app-diskman
-rm -rf feeds/luci/applications/luci-app-diskman
-git clone --depth=1 https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
-
-# '添加luci-app-mosdns
-# git clone --depth=1 https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
+https://github.com/lisaac/luci-app-diskman.git package/lean/luci-app-diskman
 
 # 使用原始最新版本
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
@@ -58,44 +48,45 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-
 # git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
 # git clone https://github.com/YL2209/luci-theme-argon-lr.git package/lean/luci-theme-argon-lr
 
-# 修改 argon 为默认主题
+# '去除默认bootstrap主题'
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# '修改默认主题为argonne'
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci-nginx/Makefile
 
 # 修改插件名字
-# sed -i 's/"挂载点"/"磁盘挂载"/g' `grep "挂载点" -rl ./`
-# sed -i 's/"Argonne 主题设置"/"主题设置"/g' `grep "Argonne 主题设置" -rl ./`
-## sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' `grep "阿里云盘 WebDAV" -rl ./`
-# sed -i 's/"状态"/"系统状态"/g' `grep "状态" -rl ./`
-# sed -i 's/"系统"/"系统设置"/g' `grep "系统" -rl ./`
-# sed -i 's/"Hello World"/"世界你好"/g' `grep "Hello World" -rl ./`
-# sed -i 's/"iKoolProxy 滤广告"/"广告屏蔽"/g' `grep "iKoolProxy 滤广告" -rl ./`
-# sed -i 's/"DDNSTO 远程控制"/"远程控制"/g' `grep "DDNSTO 远程控制" -rl ./`
-# sed -i 's/"网络存储"/"存储设置"/g' `grep "网络存储" -rl ./`
-# sed -i 's/"重启"/"系统重启"/g' `grep "重启" -rl ./`
-# sed -i 's/"服务"/"应用服务"/g' `grep "服务" -rl ./`
-# sed -i 's/"CPU 性能优化调节"/"CPU 设置"/g' `grep "CPU 性能优化调节" -rl ./`
-# sed -i 's/"网络"/"网络设置"/g' `grep "网络" -rl ./`
-# sed -i 's/"接口"/"接口设置"/g' `grep "接口" -rl ./`
-# sed -i 's/"无线"/"无线设置"/g' `grep "无线" -rl ./`
-# sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
-# sed -i 's/"实时流量监测"/"流量监测"/g' `grep "实时流量监测" -rl ./`
-# sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
+sed -i 's/"挂载点"/"磁盘挂载"/g' `grep "挂载点" -rl ./`
+sed -i 's/"Argonne 主题设置"/"主题设置"/g' `grep "Argonne 主题设置" -rl ./`
+# sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' `grep "阿里云盘 WebDAV" -rl ./`
+sed -i 's/"状态"/"系统状态"/g' `grep "状态" -rl ./`
+sed -i 's/"系统"/"系统设置"/g' `grep "系统" -rl ./`
+sed -i 's/"Hello World"/"世界你好"/g' `grep "Hello World" -rl ./`
+sed -i 's/"iKoolProxy 滤广告"/"广告屏蔽"/g' `grep "iKoolProxy 滤广告" -rl ./`
+sed -i 's/"DDNSTO 远程控制"/"远程控制"/g' `grep "DDNSTO 远程控制" -rl ./`
+sed -i 's/"网络存储"/"存储设置"/g' `grep "网络存储" -rl ./`
+sed -i 's/"重启"/"系统重启"/g' `grep "重启" -rl ./`
+sed -i 's/"服务"/"应用服务"/g' `grep "服务" -rl ./`
+sed -i 's/"CPU 性能优化调节"/"CPU 设置"/g' `grep "CPU 性能优化调节" -rl ./`
+sed -i 's/"网络"/"网络设置"/g' `grep "网络" -rl ./`
+sed -i 's/"接口"/"接口设置"/g' `grep "接口" -rl ./`
+sed -i 's/"无线"/"无线设置"/g' `grep "无线" -rl ./`
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
+sed -i 's/"实时流量监测"/"流量监测"/g' `grep "实时流量监测" -rl ./`
+sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
 # sed -i 's/"TTYD 终端"/"命令窗"/g' `grep "TTYD 终端" -rl ./`
-# sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
-## sed -i 's/"管理权"/"密码设置"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
-## sed -i 's/解锁网易云灰色歌曲/音乐解锁/g' feeds/luci/applications/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
-## sed -i 's/TTYD 终端/超级终端/g' feeds/luci/applications/luci-app-ttyd/po/zh-cn/terminal.po
-## 设置ttyd免帐号登录
-# sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
+sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
+sed -i 's/"管理权"/"密码设置"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/解锁网易云灰色歌曲/音乐解锁/g' feeds/luci/applications/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+sed -i 's/TTYD 终端/超级终端/g' feeds/luci/applications/luci-app-ttyd/po/zh-cn/terminal.po
+# 设置ttyd免帐号登录
+sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
 
 #'修改WIFI国家区域'
 sed -i 's/US/CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #修改默认无线名称
-sed -i 's/ImmortalWrt/KKT/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/KKT/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #开启MU-MIMO
 sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -110,8 +101,7 @@ sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=KKT-$(shell date +%Y
 sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # '修改概览里时间显示为中文数字'
-# sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
 
 # '首页增加CPU频率动态显示'
-# cp -rf $GITHUB_WORKSPACE/diy/mod-index.htm ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-
+cp -rf $GITHUB_WORKSPACE/diy/mod-index.htm ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
