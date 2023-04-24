a157ec3d2f51da27fdfb92c06282e23ac9f55630
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-04-24 19:13:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 
- echo 'src-git kenzo https://github.com/wwz09/openwrt-packages' >>feeds.conf.default
+ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
  echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
