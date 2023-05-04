c4973a8c15479b7fc0678bacad7594f7c5e8d5a1
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-05-04 14:29:09 +0800

    Add files via upload

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -15,3 +15,14 @@
 
 # Enable Cache
 echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
+
+#　添加新插件
+git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+
+git clone https://github.com/gdck/luci-app-control-weburl.git package/luci-app-control-weburl
+
+svn co　https://github.com/kenzok8/openwrt-packages/trunk/ddnsto package/ddnsto
+svn co　https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ddnsto package/luci-app-ddnsto
+svn co　https://github.com/kenzok8/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
+svn co　https://github.com/wwz09/openwrt-packages/trunk/luci-app-control-webrestriction package/luci-app-control-webrestriction
+svn co　https://github.com/kenzok8/small/trunk/luci-app-vssr package/luci-app-vssr
