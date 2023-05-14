b0b1fc1d9036ba2b033abe94e913aa12c012dd77
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-05-14 15:07:50 +0800

    Update qihoo.sh

--- scripts/qihoo.sh
+++ scripts/qihoo.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+#!/bin/bash
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 修改 lan IP
+sed -i 's/192.168.1.1/192.168.8.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# Add a feed source
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# 修改主机名
+sed -i 's/OpenWrt/chainsx-bee/g' openwrt/package/base-files/files/bin/config_generate
 
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# 修改连接数
+sed -i 's/16384/65535/g' openwrt/package/kernel/linux/files/sysctl-nf-conntrack.conf
 
-# '修改默内核'
-# sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/ipq40xx/Makefile         # ipq40xx，内核：5.4，内核为5.10
+# 修改 SSID
+sed -i 's/OpenWrt/chainsx-bee/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
+# 修改 wifi 地区
+sed -i 's/US/CN/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
