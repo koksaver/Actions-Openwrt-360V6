7e6d279f5847212015f061c79ac4b50ea6c3a552
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-05-14 15:10:48 +0800

    Update qihoo.sh

--- scripts/qihoo.sh
+++ scripts/qihoo.sh
@@ -5,10 +5,10 @@
 git clone https://github.com/wwz09/IPQ6108.git package/IPQ6108
 
 #　web登陆密码从password修改为空
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/addition/default-settings/files/99-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/extra/default-settings/files/99-default-settings
 
 #　固件版本号添加个人标识和日期
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/addition/default-settings/files/99-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/extra/default-settings/files/99-default-settings
 
 #　编译的固件文件名添加日期
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
