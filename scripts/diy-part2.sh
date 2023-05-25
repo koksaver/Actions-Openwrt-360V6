3ba3686c031a59f89ec052bb1db3aea0c4c8f3eb
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-05-25 15:26:27 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -17,7 +17,8 @@
 echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
 
 #　添加新插件
-git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+
+# git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 
 git clone https://github.com/wwz09/IPQ6108.git package/IPQ6108
 
@@ -27,10 +28,10 @@ git clone https://github.com/wwz09/IPQ6108.git package/IPQ6108
 # sed -i 's/192.168.1.1/192.168.124.1/g' package/base-files/files/bin/config_generate
 
 #　web登陆密码从password修改为空
-sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' package/addition/default-settings/files/99-default-settings
+# sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' package/addition/default-settings/files/99-default-settings
 
 #　固件版本号添加个人标识和日期
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/extra/default-settings/files/99-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/addition/default-settings/files/99-default-settings
 
 #　编译的固件文件名添加日期
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
@@ -41,19 +42,4 @@ sed -i "s/hostname='OpenWrt'/hostname='Qihoo V6'/g" package/base-files/files/bin
 # 添加自动挂载磁盘脚本
 # mkdir -p files/etc/hotplug.d/block && wget -O files/etc/hotplug.d/block/30-usbmount https://raw.githubusercontent.com/fichenx/P3TERX_Actions-OpenWrt/main/files/etc/hotplug.d/block/30-usbmount && chmod 755 files/etc/hotplug.d/block/30-usbmount
 
-rm -rf package/qca/qca-nss-clients
-rm -rf package/qca/qca-nss-crypto
-rm -rf package/qca/qca-nss-dp
-rm -rf package/qca/qca-nss-drv
-rm -rf package/qca/qca-nss-ecm
-rm -rf package/qca/qca-ssdk
-rm -rf package/qca/qca-ssdk-shell
 
-cp -rf $GITHUB_WORKSPACE/patches/feeds.conf.default ./feeds.conf.default
-cp -rf $GITHUB_WORKSPACE/patches/qca-nss-clients package/qca/qca-nss-clients
-cp -rf $GITHUB_WORKSPACE/patches/qca-nss-crypto package/qca/qca-nss-crypto
-cp -rf $GITHUB_WORKSPACE/patches/qca-nss-dp package/qca/qca-nss-dp
-cp -rf $GITHUB_WORKSPACE/patches/qca-nss-drv package/qca/qca-nss-drv
-cp -rf $GITHUB_WORKSPACE/patches/qca-nss-ecm package/qca/qca-nss-ecm
-cp -rf $GITHUB_WORKSPACE/patches/qca-ssdk package/qca/qca-ssdk
-cp -rf $GITHUB_WORKSPACE/patches/qca-ssdk-shell package/qca/qca-ssdk-shell
