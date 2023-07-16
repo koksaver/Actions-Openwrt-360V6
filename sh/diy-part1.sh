31889a867099d46e0571d99ca94e90a0147a66ca
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-07-16 19:22:40 +0800

    Update diy-part1.sh

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git argon https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
+# echo 'src-git argon https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
-echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
+# echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
+# echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
