8045276c7d29b4d75cd91fb6fecafa148f81cf65
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-07-16 19:25:37 +0800

    Update diy-part2.sh

--- sh/diy-part2.sh
+++ sh/diy-part2.sh
@@ -9,6 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
