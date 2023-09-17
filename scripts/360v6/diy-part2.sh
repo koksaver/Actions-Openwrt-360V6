316d3acf34e9f77419bd9141bc199923bc29f088
wwz09 <wwz5124@gmail.com>
2023-09-17 17:44:03 +0800

    update

--- scripts/360v6/diy-part2.sh
+++ scripts/360v6/diy-part2.sh
@@ -25,7 +25,7 @@ echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.10.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #　web登陆密码从password修改为空
 # sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' package/addition/default-settings/files/99-default-settings
