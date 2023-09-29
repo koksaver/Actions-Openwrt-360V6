c89f69d1e3a208714332cf95a87b9336df59d86a
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-09-29 14:13:04 +0800

    更新 diy-part2.sh

--- scripts/test/diy-part2.sh
+++ scripts/test/diy-part2.sh
@@ -36,12 +36,14 @@ sed -i 's/"挂载点"/"磁盘挂载"/g' `grep "挂载点" -rl ./`
 sed -i 's/"解锁网易云灰色歌曲"/"音乐解锁"/g' `grep "解锁网易云灰色歌曲" -rl ./`
 sed -i 's/"状态"/"系统状态"/g' `grep "状态" -rl ./`
 sed -i 's/"接口"/"有线设置"/g' `grep "接口" -rl ./`
+sed -i 's/"iStore"/"软件中心"/g' `grep "iStore" -rl ./`
+sed -i 's/"ShadowSocksR Plus+"/"你好世界"/g' `grep "ShadowSocksR Plus+" -rl ./`
 sed -i 's/"无线"/"无线设置"/g' `grep "无线" -rl ./`
 sed -i 's/"SQM QoS"/"队列管理"/g' `grep "SQM QoS" -rl ./`
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
 sed -i 's/"TTYD 终端"/"超级终端"/g' `grep "TTYD 终端" -rl ./`
 sed -i 's/"系统"/"系统设置"/g' `grep "系统" -rl ./`
-sed -i 's/"Hello World"/"世界你好"/g' `grep "Hello World" -rl ./`
+# sed -i 's/"Hello World"/"世界你好"/g' `grep "Hello World" -rl ./`
 sed -i 's/"广告屏蔽大师 Plus+"/"广告屏蔽"/g' `grep "广告屏蔽大师 Plus+" -rl ./`
 sed -i 's/"DDNSTO 远程控制"/"远程控制"/g' `grep "DDNSTO 远程控制" -rl ./`
 sed -i 's/"网络存储"/"存储设置"/g' `grep "网络存储" -rl ./`
