4042590d528a450d55c18d64c06dc02626382662
koksaver <908960222@qq.com>
2023-12-07 02:58:06 +0800

    Update diy-part2.sh

--- scripts/360V6/diy-part2.sh
+++ scripts/360V6/diy-part2.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git 360v6 https://github.com/wwz09/360v6-packages' >>feeds.conf.default
