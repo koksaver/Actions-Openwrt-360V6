dbfd6ccac0381897cb12e913dd9b327e91c1164d
koksaver <908960222@qq.com>
2023-12-07 21:52:02 +0800

    Update 360V6-chun.sh

--- scripts/360V6/360V6-chun.sh
+++ scripts/360V6/360V6-chun.sh
@@ -17,7 +17,7 @@
 echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
 
 # '修改默认主机名'
-sed -i 's/Portable/360V6/g' package/addition/default-settings/files/99-default-settings
+sed -i 's/Portable/360V6/g' package/extra/default-settings/files/99-default-settings
 
 #　添加新插件
 
@@ -57,10 +57,10 @@ sed -i 's/"MWAN3 分流助手"/"分流助手"/g' `grep "MWAN3 分流助手" -rl 
 sed -i 's/192.168.10.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 #　web登陆密码从password修改为空
-# sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' package/addition/default-settings/files/99-default-settings
+# sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' package/extra/default-settings/files/99-default-settings
 
 #　固件版本号添加个人标识和日期
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/addition/default-settings/files/99-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/extra/default-settings/files/99-default-settings
 
 #　编译的固件文件名添加日期
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=360V6chunjinban-$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
