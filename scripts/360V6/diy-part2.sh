8b6a9d70c465f95e4e827b388fb2b49a35d04f47
koksaver <908960222@qq.com>
2024-01-02 11:33:53 +0800

    Update diy-part2.sh

--- scripts/360V6/diy-part2.sh
+++ scripts/360V6/diy-part2.sh
@@ -19,4 +19,5 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>fee
 echo 'src-git MyPackages https://github.com/koksaver/MyOpenWrtPackages' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >>feeds.conf.default
+echo 'src-git adguardhome https://github.com/xiaoxiao29/luci-app-adguardhome;master' >>feeds.conf.default
 #echo 'src-git 360v6 https://github.com/wwz09/360v6-packages' >>feeds.conf.default
